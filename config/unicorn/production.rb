bd82fa2d759d483ccdb81068d5df9fc7d1019ab6
Stefan Wienert <stwienert@gmail.com>
2013-02-21 14:36:59 +0100

    ...

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -24,7 +24,7 @@ stdout_path "log/unicorn.log"
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 before_fork do |server, worker|
-  ActiveRecord::Base.connection.disconnect!
+  #ActiveRecord::Base.connection.disconnect!
 
   old_pid = "#{server.config[:pid]}.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
@@ -37,5 +37,5 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
+  #ActiveRecord::Base.establish_connection
 end
