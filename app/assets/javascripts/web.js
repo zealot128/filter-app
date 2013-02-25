b6ed8944b6e5e977284be7c9fa42def8e9b7bb49
Stefan Wienert <stwienert@gmail.com>
2013-02-25 10:13:49 +0100

    html5slider

--- app/assets/javascripts/web.js
+++ app/assets/javascripts/web.js
@@ -1,6 +1,6 @@
 //= require jquery
 //= require jquery.timeago
+//= require html5slider
 //= require twitter/bootstrap/dropdown
-//= require fd-slider.min.js
 //= require jquery.tinysort
 //= require_tree ./web
