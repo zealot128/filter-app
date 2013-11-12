02cfb9e8b46d126160ab87c7a0bd7034728eda37
Stefan Wienert <stwienert@gmail.com>
2013-11-12 10:57:38 +0100

    cron log

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,21 +1,9 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+set :output, "/apps/hrcollect/prod/current/log/cron_log.log"
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
 every 15.minutes do
   runner "Source.cronjob"
 end
+
 every 1.hour do
   runner ' NewsItem.cronjob'
 end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
