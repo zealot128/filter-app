c586cfc8984bc9b04b1c45d2ddbd0d391fcc0b40
Stefan Wienert <stefan.wienert@pludoni.de>
2015-06-01 17:38:12 +0200

    Suche mehrspaltig, I18n fuer willpaginate

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,7 +1,9 @@
 class NewsItemsController < ApplicationController
   def index
     if params[:q].present?
-      @news_items = NewsItem.search_full_text(params[:q]).paginate(page: params[:page])
+      @news_items = NewsItem.
+        where('published_at > ?', 2.years.ago).
+        search_full_text(params[:q]).paginate(page: params[:page])
     end
   end
 end
