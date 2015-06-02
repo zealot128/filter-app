f04ac2c20f9dd9b6343c165a4e62d0adf6c90074
Stefan Wienert <stefan.wienert@pludoni.de>
2015-06-02 15:37:14 +0200

    Cronjob seltener

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,13 @@
 set :output, "/var/www/hrfilter.de/shared/log/cron.log"
 job_type :runner, "cd :path && bin/rails runner -e :environment ':task' :output"
 
-every 15.minutes do
-  runner "Source.cronjob"
-end
 
 every 1.hour do
   runner ' NewsItem.cronjob'
+end
+
+every 1.hour do
+  runner "Source.cronjob"
 end
 
 every 1.day do
