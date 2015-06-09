7fb9092f0a8de2d0fc5b747bcc14325a9c342a4a
Stefan Wienert <stefan.wienert@pludoni.de>
2015-06-10 00:26:59 +0200

    ..

--- app/processors/tweet_processor.rb
+++ app/processors/tweet_processor.rb
@@ -10,6 +10,7 @@ class TweetProcessor < Processor
       .transcript
       #articleContent
       #content
+      [itemprop=articleBody]
       .postContent
       .hcf-content
       .entryContent
@@ -17,6 +18,7 @@ class TweetProcessor < Processor
       .post
       .entry
       article
+      .article
       main
       section
     ]
