56e811d5e2b18d71d0bcc840f35590bee9eda048
Stefan Wienert <stefan.wienert@pludoni.de>
2015-06-24 23:00:26 +0200

    RSS Feeds fuer Suche; Suche hat 3 Sortierungen

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,9 +1,29 @@
 class NewsItemsController < ApplicationController
+
   def index
+    minscore = 0.5
     if params[:q].present?
+      @feed_url = search_url(q: params[:q], sort: params[:sort], format: :rss)
       @news_items = NewsItem.
         where('published_at > ?', 2.years.ago).
-        search_full_text(params[:q]).paginate(page: params[:page])
+        includes(:source, :categories).
+        search_full_text(params[:q]).
+        where('pg_search_news_items.rank > ?', minscore)
+      case params[:sort]
+      when 'freshness'
+        @news_items = @news_items.reorder('published_at desc')
+      when 'score'
+        @news_items = @news_items.reorder('absolute_score desc')
+      end
+
+      respond_to do |f|
+        f.html {
+          @news_items = @news_items.paginate(page: params[:page], per_page: 40)
+        }
+        f.rss {
+          @news_items = @news_items.limit(50)
+        }
+      end
     end
   end
 
@@ -14,7 +34,6 @@ class NewsItemsController < ApplicationController
         @news_items = @news_items.joins('LEFT JOIN "categories_news_items" ON "categories_news_items"."news_item_id" = "news_items"."id"').
           where('news_item_id is null').
           group('news_items.id')
-
       else
         @news_items =
           @news_items.joins(:categories).where(categories: { id: params[:category] }).group('news_items.id')
---
+++ app/views/news_items/index.rss.builder
@@ -0,0 +1,21 @@
+xml.instruct!
+xml.rss(version: "2.0",
+        "xmlns:atom"=>"http://www.w3.org/2005/Atom") do
+  xml.channel do
+    xml.title "HR-Filter Feed für Suche nach #{params[:q]}"
+    xml.link search_url(q: params[:q], sort: params[:sort])
+    xml.lastBuildDate Time.now
+    xml.tag!('atom:link', rel: 'self', type: 'application/rss+xml', href: search_url(q: params[:q], sort: params[:sort]))
+    @news_items.each do |ni|
+      xml.item do
+        xml.title ni.title
+        xml.link ni.url
+        xml.guid ni.guid
+        xml.description do
+          xml.cdata! ni.teaser
+        end
+      end
+    end
+  end
+end
+
