a61a8b3162fa8bee5228bf78b7f6b86992b734f6
Stefan Wienert <stefan.wienert@pludoni.de>
2015-06-24 23:28:05 +0200

    DupKiller

---
+++ app/services/duplicate_killer.rb
@@ -0,0 +1,15 @@
+class DuplicateKiller
+  attr_reader :source
+  def initialize(source)
+    @source = source
+  end
+
+  def run
+    dups = source.news_items.group("regexp_replace(url, 'https?:', '')").having('count(*) > 1').select('array_agg(id) as ids').map{|i| i.ids}
+    dups.each do |group|
+      nis = NewsItem.where(id: group).order('absolute_score desc').pluck(:id).to_a
+      nis.shift
+      NewsItem.where(id: nis).delete_all
+    end
+  end
+end
