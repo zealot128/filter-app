d737361ed47f8f692f0209c469a24ffb6f096baf
Stefan Wienert <stefan.wienert@pludoni.de>
2015-09-13 23:13:42 +0200

    Image Fetcher: blogger image_src supported

---
+++ app/services/news_item/image_fetcher.rb
@@ -0,0 +1,23 @@
+class NewsItem::ImageFetcher
+  attr_reader :news_item, :page
+  def initialize(news_item, page)
+    @news_item = news_item
+    @page = page
+  end
+
+  def run
+    return if news_item.image.present?
+
+    image = page.parser.xpath('//meta[@property="og:image" or @name="shareaholic:image"]').first || page.parser.xpath('//link[@rel="image_src"]').first
+    if image
+      url = image['content'] || image['href']
+      begin
+        image = download_url(url)
+        news_item.update_attributes image: image
+      rescue SocketError, StandardError => e
+        Rails.logger.error "OG image download fehlgeschlagen #{url} #{e.inspect}"
+      end
+    end
+  end
+
+end
