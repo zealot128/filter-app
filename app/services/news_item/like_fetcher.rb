d60c518cf9e6c52482f86d6da066d21e4814b465
Stefan Wienert <stefan.wienert@pludoni.de>
2015-09-16 10:15:19 +0200

    FB Like Fetcher - Fallback Like button

--- app/services/news_item/like_fetcher.rb
+++ app/services/news_item/like_fetcher.rb
@@ -8,7 +8,10 @@ class NewsItem::LikeFetcher
   def self.fetch_for_news_item(news_item)
     fetcher = new(news_item.url)
     news_item.retweets = fetcher.tweets
-    news_item.fb_likes = fetcher.facebook
+    f = fetcher.facebook
+    if f
+      news_item.fb_likes = f
+    end
     news_item.linkedin = fetcher.linkedin
     news_item.xing     = fetcher.xing
     news_item.gplus    = fetcher.gplus
@@ -25,6 +28,9 @@ class NewsItem::LikeFetcher
     json = JSON.parse Fetcher.fetch_url(
       "https://api.facebook.com/method/fql.query?query=select%20%20url,like_count,%20total_count,%20share_count,%20click_count%20from%20link_stat%20where%20url%20=%20%22#{eurl}%22&format=json").body
     json[0]["total_count"]
+  rescue
+    body = Fetcher.fetch_url "https://www.facebook.com/v2.3/plugins/like.php?action=recommend&app_id=113869198637480&channel=https%3A%2F%2Fs-static.ak.facebook.com%2Fconnect%2Fxd_arbiter%2F44OwK74u0Ie.js%3Fversion%3D41%23cb%3Df232c6343e45cbe%26domain%3Ddevelopers.facebook.com%26origin%3Dhttps%253A%252F%252Fdevelopers.facebook.com%252Ff29ef60e417f34%26relation%3Dparent.parent&container_width=588&href=#{eurl}&locale=de_DE&sdk=joey&share=true&show_faces=true"
+    body.body.to_s[/(\d+) Person(en)? empfehlen das/, 1].to_i
   end
 
   def linkedin
