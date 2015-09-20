dc73252822c7bb8cd02eeb7f6ef82f296c546c37
Stefan Wienert <stefan.wienert@pludoni.de>
2015-09-20 19:58:00 +0200

    Cronjob

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,5 @@
-set :output, "/var/www/hrfilter.de/shared/log/cron.log"
+set :output, "#{Dir.pwd}/shared/log/cron.log"
 job_type :runner, "cd :path && bin/rails runner -e :environment ':task' :output"
-
 
 every 1.hour do
   runner 'NewsItem.cronjob'
