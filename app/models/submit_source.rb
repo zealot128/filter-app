cca783b585c2252995c7b72b3f1a010ad629102d
tobma <tobiasmarkert@gmx.de>
2015-09-24 15:49:54 +0200

    Added submit source page

---
+++ app/models/submit_source.rb
@@ -0,0 +1,15 @@
+class SubmitSource < MailForm::Base
+  attribute :url,       :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :comment
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "[HRfilter] Neue Quelle eingesendet",
+      :to => "tobiasmarkert@gmx.de",
+      :from => %("#{email}")
+    }
+  end
+end
