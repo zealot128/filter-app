8cb2f354e980dd8414deda8c58f11c9fa952f53a
Stefan Wienert <stefan.wienert@pludoni.de>
2015-10-06 17:04:22 +0200

    source page: show more than recent items

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -11,14 +11,6 @@ class SourcesController < ApplicationController
     @count = @source.news_items.visible.count
     @avg = @count / @source.age_in_weeks
 
-    @news_items = @source.news_items.current.order('value desc')
-    if @news_items.count < 50
-      other = @source.news_items.
-              visible.
-              order('absolute_score desc').
-              where('news_items.id not in (?)', @news_items.map(&:id).presence || [-1]).
-              limit(50 - @news_items.count)
-      @news_items += other
-    end
+    @news_items = @source.visible.news_items.order('published_at desc').limit(100)
   end
 end
