a94c72c040501cd9ed7ec15c36b8974dc776e619
Stefan Wienert <stefan.wienert@pludoni.de>
2016-01-21 14:31:36 -0500

    on source show page: show link to homepage url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def page_title
     [ @title, Configuration.site_name].reject(&:blank?).join(' | ')
   end
+  def homepage(url)
+    URI.parse(url).tap{|o| o.path = '/'; o.query =nil}.to_s
+  end
 end
