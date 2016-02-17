e0d9987fbb5c8cca05732f4f20823a1ff642b2eb
Stefan Wienert <stefan.wienert@pludoni.de>
2016-02-17 18:30:30 +0100

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter do
-    if request.host == ::Configuration.host.remove('www.', '')
+    if request.host == ::Configuration.host.remove('www.', '') and Rails.env.production?
       url = "http://#{::Configuration.host}" + request.env['REQUEST_URI']
       redirect_to url, status:  :moved_permanently
     end
