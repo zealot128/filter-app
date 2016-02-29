d82a3c152736ba2327261bcfc74aabd71ed341c7
Stefan Wienert <stefan.wienert@pludoni.de>
2016-02-29 09:27:27 +0100

    JS fix on admin page

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,10 +1,9 @@
-//= require jquery_ujs
 //= require jquery.tablesorter.min
 //= require_self
 //
 
 $(document).ready(function() {
   $('.js-tablesorter').each(function() {
-    $(this).tablesorter()
+    $(this).tablesorter();
   });
-})
+});
