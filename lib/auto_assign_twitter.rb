77e8ff103629a34c95bba4e91c98f551356a39b9
Stefan Wienert <stefan.wienert@pludoni.de>
2016-03-09 21:07:41 +0100

    Twitter Account finder also uses home page of blogs

--- lib/auto_assign_twitter.rb
+++ lib/auto_assign_twitter.rb
@@ -14,21 +14,30 @@ class AutoAssignTwitter < Processor
     return if @source.twitter_account
     return if count > 3
     return if !news_item
-    s = get news_item.url
-    if intent = s.links.select{|i| i.href.to_s['https://twitter.com/intent/follow']}.first
-      assign_account  CGI.parse(URI.parse(intent.href).query)['screen_name'].first
-      return
+    if count == 0
+      base_url = URI.parse(news_item.url).tap{|q| q.path = '/'}.to_s
+      page = get base_url
+      find_link_on_page(page)
     end
-    if intent = s.links.select{|i| i.href.to_s[%r{twitter.com/(#\!/)?\w+/?$}]}.first
-      acc = intent.href.split('/').last
-      return if acc[/share/]
-      assign_account acc
-      return
-    end
+    return if @source.twitter_account
+
+    page = get news_item.url
+    find_link_on_page(page)
+    return if @source.twitter_account
     puts "Nichts gefunden #{@source.name}"
   rescue Mechanize::ResponseCodeError, SocketError => e
     puts "FEHLER bei #{@source.name} -> #{e.inspect}"
     run(news_item: @source.news_items.order('created_at desc')[count + 1], count: count + 1)
+  end
+
+  def find_link_on_page(page)
+    if intent = page.links.select{|i| i.href.to_s['https://twitter.com/intent/follow']}.first
+      assign_account  CGI.parse(URI.parse(intent.href).query)['screen_name'].first
+    end
+    if intent = page.links.select{|i| i.href.to_s[%r{twitter.com/(#\!/)?\w+/?$}] && !i.href[/share/]}.first
+      acc = intent.href.split('/').last
+      assign_account acc
+    end
   end
 
   private
