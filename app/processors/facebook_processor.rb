d14a7ab5040d92e5fa172552f83702c7ae0f5e0b
Stefan Wienert <stefan.wienert@pludoni.de>
2016-08-01 13:55:32 +0200

    Facebook rendering

--- app/processors/facebook_processor.rb
+++ app/processors/facebook_processor.rb
@@ -16,31 +16,23 @@ class FacebookProcessor < Processor
 
     url = link['onmouseover'].split('"')[1].gsub("\\/", "/")
 
-
     news_item = source.news_items.where(guid: url).first_or_initialize
-    news_item.title = link.text
+    news_item.title = parent.at('._5s6c a').text
 
     if news_item.new_record?
       new_m = Mechanize.new
       new_m.get(url)
       news_item.url = new_m.page.uri.to_s
     end
-    if news_item.full_text.blank?
-      news_item.full_text, = get_full_text_and_image_from_random_link(url)
-    end
 
     text = parent.at('.userContent').text
     filteredText = text.gsub(/[^\p{Word} #\b\.\,\/:]/, '').gsub(/ +/, ' ')
     news_item.teaser = filteredText
-
     news_item.published_at = Time.at parent.at('.timestampContent').parent['data-utime'].to_i
-
-    if news_item.image.blank?
-      if img = parent.at('.fbStoryAttachmentImage img')
-        news_item.image = download_url(img['src'])
-      end
+    news_item.rescore!
+    if !news_item.full_text.present?
+      NewsItem::FullTextFetcher.new(news_item, unknown_selector: true).run
     end
-    news_item.save
   end
 
 end
