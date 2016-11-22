c217578bb6f415a6d7da0087bc5b9ff7ae8caa8c
Stefan Wienert <stefan.wienert@pludoni.de>
2016-11-22 10:00:45 +0100

    More bots blocked

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,10 +1,10 @@
 Rack::Attack.blocklist('fail2ban pentesters') do |req|
   # `filter` returns truthy value if request fails, or if it's from a previously banned IP
   # so the request is blocked
-  Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 5.minutes) do
+  Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 15.minutes) do
 		qs = CGI.unescape(req.query_string)
     #  # The count for the IP is incremented if the return value is truthy
-    qs =~ %r{/etc/passwd|/proc/self|/etc/hosts} ||
+    qs.include?("'A=0") || qs =~ %r{/etc/passwd|/proc/self|/etc/hosts} ||
       req.path.include?('/etc/passwd') ||
       req.path.include?('/etc/passwd') ||
       req.path.include?('wp-admin') ||
