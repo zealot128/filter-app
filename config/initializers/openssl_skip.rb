8d3fc12cb2dfc573af245f7e0bdc63ac83acd5d6
Stefan Wienert <info@stefanwienert.de>
2017-02-22 22:30:10 +0100

    ^^

--- config/initializers/openssl_skip.rb
+++ config/initializers/openssl_skip.rb
@@ -1,2 +1,3 @@
+I_KNOW_THAT_OPENSSL_VERIFY_PEER_EQUALS_VERIFY_NONE_IS_WRONG = nil
 # UNfortunately lot of broken ssl installations, wrong domain
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
