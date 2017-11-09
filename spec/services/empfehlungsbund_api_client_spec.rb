f728e7109eb7ef06fe801a0baeae449c39fb647e
Wolfgang Wohanka <wolfgang.wohanka@pludoni.de>
2017-11-09 13:50:39 +0100

    Zeile überflüßig

--- spec/services/empfehlungsbund_api_client_spec.rb
+++ spec/services/empfehlungsbund_api_client_spec.rb
@@ -14,7 +14,6 @@ describe EmpfehlungsbundApiClient do
   specify 'Community Events', freeze_time: "2017-08-01 12:00" do
     VCR.use_cassette 'eb_api_client/community_events' do
       events = EmpfehlungsbundApiClient.community_events
-      events.select { |i| i['visible'] && i['start'] >= Time.zone.now.to_s }
       expect(events.count).to be >= 1
       event = events.first
       expect(event.url).to be_present
