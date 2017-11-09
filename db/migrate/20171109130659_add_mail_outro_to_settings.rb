e0264984ad7ad362533215a126d73062ee1cb789
Wolfgang Wohanka <wolfgang.wohanka@pludoni.de>
2017-11-09 13:24:53 +0100

    * Leerzeile für mail_outro * freeze_time für Community-Event Test um Ergebnisse zu garantieren

--- db/migrate/20171109130659_add_mail_outro_to_settings.rb
+++ db/migrate/20171109130659_add_mail_outro_to_settings.rb
@@ -4,7 +4,7 @@ class AddMailOutroToSettings < ActiveRecord::Migration
 
   def data
     Setting.where(key: 'mail_outro').first_or_create(value: <<-DOC.strip_heredoc)
-		Die Newsletter Einstellungen können <a href='/newsletter'>hier</a> vorgenommen werden.</br>Haben Sie Anregungen, dann kontaktieren Sie mich unter {{person_email}}</br>Viele Grüße</br>{{person}}
+		Die Newsletter Einstellungen können <a href='/newsletter'>hier</a> vorgenommen werden.<br/><br/>Haben Sie Anregungen, dann kontaktieren Sie mich unter {{person_email}}.<br/><br/>Viele Grüße<br/><br/>{{person}}
 	DOC
   end
 end
