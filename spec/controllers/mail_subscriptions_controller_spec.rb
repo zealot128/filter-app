028abc25862d2e6a2322564704fb60096bc3595a
Wolfgang Wohanka <wolfgang.wohanka@pludoni.de>
2017-11-12 12:54:28 +0100

    Testfix

--- spec/controllers/mail_subscriptions_controller_spec.rb
+++ spec/controllers/mail_subscriptions_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe MailSubscriptionsController do
+  before do
+    Rails.cache.clear
+  end
+
   let(:subscription) {
     {
       email: 'stwienert@gmail.com',
@@ -14,20 +18,18 @@ describe MailSubscriptionsController do
   }
 
   specify 'newsletter after subscription after 9am Monday' do
-    VCR.use_cassette 'subscription' do
+    VCR.use_cassette 'events' do
       ms = MailSubscription.create!(subscription)
       post :confirm, id: ms.token
       expect(ActionMailer::Base.deliveries.count).to eq(1)
     end
   end
 
-  specify 'no newsletter after subscription on Monday before 9am' do
-    VCR.use_cassette 'subscription' do
-      Timecop.travel(Time.zone.local(2017, 11, 6, 8, 0, 0)) do
-        ms = MailSubscription.create!(subscription)
-        post :confirm, id: ms.token
-        expect(ActionMailer::Base.deliveries.count).to eq(0)
-      end
+  specify 'no newsletter after subscription on Monday before 9am', freeze_time: "2017-11-06 8:00" do
+    VCR.use_cassette 'events' do
+      ms = MailSubscription.create!(subscription)
+      post :confirm, id: ms.token
+      expect(ActionMailer::Base.deliveries.count).to eq(0)
     end
   end
 end
