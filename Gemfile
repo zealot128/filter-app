ba4392773cf59ab3a30b4bc905f322b5075dd55b
Stefan Wienert <info@stefanwienert.de>
2018-09-24 15:08:52 +0200

    Rails 5.2 + ActiveStorage

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-gem "rails", "~> 5.1.0"
+gem "rails", "~> 5.2.0"
 gem "sprockets-es6"
 gem "pg"
 gem "mail_form"
--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,13 @@ module Baseapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    ###
     config.i18n.default_locale = :de
     config.i18n.locale = :de
     config.encoding = "utf-8"
