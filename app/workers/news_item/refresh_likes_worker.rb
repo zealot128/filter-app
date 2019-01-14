836c19a82c285fad08cbedc4509609cbdd1b8d69
Stefan Wienert <info@stefanwienert.de>
2019-01-14 14:36:43 +0100

    error fix

--- app/workers/news_item/refresh_likes_worker.rb
+++ app/workers/news_item/refresh_likes_worker.rb
@@ -2,7 +2,7 @@ class NewsItem::RefreshLikesWorker
   include Sidekiq::Worker
 
   def perform(news_item_id)
-    news_item = Newsitem.find(news_item_id)
+    news_item = NewsItem.find(news_item_id)
     news_item.refresh
     NewsItem::RefreshStatsWorker.perform_async(ni.id)
   end
