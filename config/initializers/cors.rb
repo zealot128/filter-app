cff7e99cbac71d22e227c7083531eceba3469eb5
Laurens Witt <laurens.witt@pludoni.de>
2019-03-07 13:05:27 +0100

    Fix Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,13 @@
 Rails.application.configure do
-  config.middleware.insert_before 0, Rack::Cors, :debug => true, :logger => ::Logger.new(STDOUT) do
+  config.middleware.insert_before 0, Rack::Cors, debug: true, logger: ::Logger.new(STDOUT) do
     allow do
       if Rails.env.development?
-        origins "pludoni.#{ENV['USER']}.pludoni.com"
+        origins "pludoni.#{ENV['LOGNAME']}.pludoni.com"
       else
-        origins 'pludoni.de'
+        origins 'www.pludoni.de'
       end
-      resource '/api/v1/categories.json', headers: :any, methods: :any, credentials: true
-      resource '/api/v1/news_items.json?category_id=1&limit=10', headers: :any, methods: :any, credentials: true
+      resource '/api/v1/categories.json', headers: :any, methods: :get
+      resource '/api/v1/news_items.json', headers: :any, methods: :get
     end
   end
 end
