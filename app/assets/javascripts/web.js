f8d8c6cb923665dc2a430b3beb9b1bb42799f5d8
Laurens Witt <laurens.witt@pludoni.de>
2019-04-23 10:13:07 +0200

    Wieder nur einzelne Bootstrap Module

--- app/assets/javascripts/web.js
+++ app/assets/javascripts/web.js
@@ -2,9 +2,11 @@
 //= require owl.carousel
 //= require jquery_ujs
 //= require jquery.timeago
-//= require bootstrap
+//= require bootstrap/dropdown
+//= require bootstrap/collapse
+//= require bootstrap/modal
+//= require bootstrap/transition
 //= require_tree ./web
 //= require highcharts
 //= require polyfill
 //= require_tree ./charts
-
