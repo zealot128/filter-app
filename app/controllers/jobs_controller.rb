f696acc876a05f01b86a2531ed1d50bc89fa8a15
Laurens Witt <laurens.witt@pludoni.de>
2019-05-08 16:00:00 +0200

    Job Location Facets mit von EB-Api abholen

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,14 @@
 class JobsController < ApplicationController
   def index
     response = Rails.cache.fetch("hrfilter.#{Setting.jobs_url}", expires_in: 10.minutes) {
-      HTTParty.get(Setting.jobs_url)['jobs']
+      resp = HTTParty.get(Setting.jobs_url + "&with_facets=true")
+      {
+        jobs: resp['jobs'],
+        facets: resp['facets']
+      }
     }
-    @jobs = response.shuffle
+    @jobs = response[:jobs].shuffle
+    @locations = response[:facets]['location_names'].keys
     render layout: false
   end
 end
