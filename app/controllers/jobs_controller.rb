bc237db1860e055d3ec95dde63764e77346d1c67
Laurens Witt <laurens.witt@pludoni.de>
2019-05-17 07:22:12 +0200

    Filter Job-Filter entfernt

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,14 +1,9 @@
 class JobsController < ApplicationController
   def index
     response = Rails.cache.fetch("hrfilter.#{Setting.jobs_url}", expires_in: 10.minutes) {
-      resp = HTTParty.get(Setting.jobs_url + "&with_facets=true")
-      {
-        jobs: resp['jobs'],
-        facets: resp['facets']
-      }
+      HTTParty.get(Setting.jobs_url)['jobs']
     }
-    @jobs = response[:jobs].shuffle
-    @locations = response[:facets]['location_names'].keys
+    @jobs = response.shuffle
     render layout: false
   end
 end
