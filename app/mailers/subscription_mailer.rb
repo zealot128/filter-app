d86524ab5c1229195add404e8fdf21ea3d06e0c2
Laurens Witt <laurens.witt@pludoni.de>
2019-08-14 16:14:30 +0200

    Scope umbenannt, Benennung automatisch

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -2,8 +2,9 @@ class SubscriptionMailer < ActionMailer::Base
   default from: Setting.get('from')
   layout 'newsletter'
 
-  def confirmation_mail(subscription)
+  def confirmation_mail(subscription, from: Setting.get('from'))
     @subscription = subscription
-    mail to: subscription.full_email, subject: "[#{Setting.site_name}] Bestätigung des E-Mail-Abos"
+    mail to: subscription.full_email, subject: "[#{Setting.site_name}] Bestätigung des E-Mail-Abos",
+      from: from
   end
 end
