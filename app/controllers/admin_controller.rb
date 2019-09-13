a31e1b4639094b716fa2488c2b3ce2f1d5f3d622
Stefan Wienert <info@stefanwienert.de>
2019-09-13 12:40:31 +0200

    Allow signing in with username/password for api purposes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,18 @@
 # :nocov:
 class AdminController < ApplicationController
+  before_action do
+    if ActionController::HttpAuthentication::Basic.has_basic_credentials?(request) and request.format.json?
+      username, password = ActionController::HttpAuthentication::Basic.user_name_and_password(request)
+      if Rails.application.secrets.http_username == username && Rails.application.secrets.http_password == password
+        sign_in User.first
+        @sign_out_after = true
+        request.env['rack.session.options'][:skip] = true
+      else
+        render status: 401, text: "Unauthenticated"
+      end
+    end
+  end
+
   before_action :authenticate_user!
   check_authorization
 
