43517caa43523c43c541299faeb56c46bf9a1f02
Stefan Wienert <info@stefanwienert.de>
2019-09-18 22:51:51 +0200

    ups

--- app/workers/source/rescore_all_worker.rb
+++ app/workers/source/rescore_all_worker.rb
@@ -3,6 +3,6 @@ class Source::RescoreAllWorker
 
   def perform(source_id)
     source = Source.find(source_id)
-    source.news_items.where('published_at > 2.years.ago').find_each(&:rescore!)
+    source.news_items.where('published_at > ?', 2.years.ago).find_each(&:rescore!)
   end
 end
