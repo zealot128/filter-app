f91b3280eeae7a4da46a0af175716d16bedad56a
Stefan Wienert <info@stefanwienert.de>
2019-10-17 11:03:48 +0200

    Fix: Salutation in inactivity mail

--- app/controllers/admin/trends/words_controller.rb
+++ app/controllers/admin/trends/words_controller.rb
@@ -4,7 +4,7 @@ class Admin::Trends::WordsController < AdminController
   def index
     @week = (params[:week] || 0).to_i.weeks.ago.strftime("%G%W")
     base = Trends::Word.all.where(ignore: false).joins(:usages).where('trends_usages.calendar_week = ?', @week).
-      group(:id).order('count desc').having('count(distinct source_id) > 2').limit(50).select("trends_words.*, count(distinct source_id) as count")
+      group(:id).order('count desc').having('count(distinct source_id) >= 2').limit(50).select("trends_words.*, count(distinct source_id) as count")
     @top_trigram = base.trigram
     @top_bigram = base.bigram
     @top_single = base.single
