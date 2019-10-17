a7afde2606e49999c20be4a81217d6ec9d745666
Stefan Wienert <info@stefanwienert.de>
2019-10-17 15:44:20 +0200

    Ci: Testfix

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -4,13 +4,13 @@ describe CategoriesController do
   specify '#index' do
     get :index
     expect(response).to be_successful
-    expect(response.body).to include "Alle Themen auf"
+    expect(response.body).to include "Alle Kategorien auf"
   end
 
   specify '#show' do
     c = Fabricate(:category)
     get :show, params: { id: c.slug }
     expect(response).to be_successful
-    expect(response.body).to include "Alle News zum Thema #{c.name}"
+    expect(response.body).to include "Alle News in der Kategorie #{c.name}"
   end
 end
