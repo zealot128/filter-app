4c5f81ccf15cce2484571a459aa63d2a286391dd
Stefan Wienert <info@stefanwienert.de>
2020-06-09 15:19:01 +0200

    TrendCloud mit 4

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -2,7 +2,7 @@ class TrendsController < ApplicationController
   # Trends of last N days
   def index
     days7 = Trends::Trend.top_of_n_days(7.days.ago, 4)
-    days30 = Trends::Trend.top_of_n_days(30.days.ago, 8)
+    days30 = Trends::Trend.top_of_n_days(30.days.ago, 4)
 
     render json: {
       'week' => days7.map { |i| i.as_json.except("updated_at", "created_at") },
