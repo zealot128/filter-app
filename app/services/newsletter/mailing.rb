02c81a19447b648a95187fbbf24b8fae09343e72
Stefan Wienert <info@stefanwienert.de>
2021-02-17 13:13:11 +0100

    Fix NoMethodError

--- app/services/newsletter/mailing.rb
+++ app/services/newsletter/mailing.rb
@@ -32,7 +32,7 @@ module Newsletter
         flat_map { |i| i.respond_to?(:news_items) ? i.news_items : [] }.
         sort_by { |i|
           source = (i.source.value || 1).clamp(1, 8)
-          -(i.value / source * Math.log(source + 1))
+          -((i.value || 0) / source * Math.log(source + 1))
         }.
         take(3)
       if top_news_items.none?
