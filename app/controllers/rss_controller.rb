6f36dd94d8640ce4b65489f99bc5c47280cdf09a
Stefan Wienert <info@stefanwienert.de>
2023-03-16 08:55:45 +0100

    Fix: Query method called with non-attribute argument(s): "to_char(published_at, YYYY/IW) desc, absolute_score desc"
    Fixes HRFILTER-6Y

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -4,22 +4,22 @@ class RssController < ApplicationController
 
   def daily_top_10
     @news_items = news_items.top_percent_per_day(14.days.ago, 0.2, 10).order('published_at::date desc, absolute_score').limit(200)
-    render 'feed.rss'
+    render 'feed', formats: [:rss]
   end
 
   def daily_top_50
     @news_items = news_items.top_percent_per_day(14.days.ago, 0.5, 30).order('published_at::date desc, absolute_score').limit(200)
-    render 'feed.rss'
+    render 'feed', formats: [:rss]
   end
 
   def weekly_top_50
-    @news_items = news_items.top_percent_per_week(4.weeks.ago, 0.2, 50).order("to_char(published_at, 'YYYY/IW') desc, absolute_score desc").limit(200)
-    render 'feed.rss'
+    @news_items = news_items.top_percent_per_week(4.weeks.ago, 0.2, 50).order(Arel.sql("to_char(published_at, 'YYYY/IW') desc, absolute_score desc")).limit(200)
+    render 'feed', formats: [:rss]
   end
 
   def newest
     @news_items = news_items.order('published_at desc').limit(200)
-    render 'feed.rss'
+    render 'feed', formats: [:rss]
   end
 
   private
