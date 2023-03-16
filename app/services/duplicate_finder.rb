260721b43c4448d3c9f1005c45d48cdd4427ed7a
Stefan Wienert <info@stefanwienert.de>
2023-03-16 08:52:32 +0100

    Fixes Query method called with non-attribute argument(s): "count(*)"
    Fixes HRFILTER-71

--- app/services/duplicate_finder.rb
+++ app/services/duplicate_finder.rb
@@ -1,6 +1,6 @@
 class DuplicateFinder
   def self.run
-    NewsItem.group(:source_id, :title).having('count(*) > 1').pluck(:source_id, :title, 'count(*)').map do |source_id, title, count|
+    NewsItem.group(:source_id, :title).having(Arel.sql('count(*) > 1')).pluck(:source_id, :title, 'count(*)').map do |source_id, title, count|
       Source.find(source_id).news_items.where(title: title).order('created_at asc').limit(count - 1).each(&:destroy)
     end
   end
