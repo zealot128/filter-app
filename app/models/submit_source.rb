a1920c8c0faf54a67115250052fb7142e7aec732
Stefan Wienert <info@stefanwienert.de>
2025-04-28 12:30:38 +0000

    reduce spam

--- app/models/submit_source.rb
+++ app/models/submit_source.rb
@@ -3,6 +3,24 @@ class SubmitSource < MailForm::Base
   attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :comment
 
+  BLOCKLIST = Regexp.union([
+    /graph\.org/,
+    /\.(ru|ua|by|kz|in)$/,
+    /testing-your-form.info/,
+    /wikidoc/,
+    /example.com/
+  ])
+
+  def deliver
+    return false unless valid?
+
+    if url.to_s[BLOCKLIST] && email.to_s[BLOCKLIST] && comment.to_s[BLOCKLIST]
+      return true
+    end
+
+    super
+  end
+
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
